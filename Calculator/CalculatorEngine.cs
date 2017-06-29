28adc782f4f4f89795f62f34c2d5ca19254b56cd
Claus Stolz <ruscoolman@gmail.com>
2017-06-30 02:50:40 +1000

    Init calculator engine

---
+++ Calculator/CalculatorEngine.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Calculator
+{
+    public enum Operator { None, Plus, Minus, Multiply, Division }
+
+    public class CalculatorEngine
+    {
+        public List<decimal> Numbers { get; set; }
+        public Operator UsedOperator { get; set; }
+        public Int32 UsedNumberIterator;    
+
+
+        public CalculatorEngine()
+        {
+            InitFields();
+        }
+
+        private void InitFields()
+        {
+            Numbers            = new List<decimal>() {0, 0};
+            UsedOperator       = Operator.None;
+            UsedNumberIterator = 0;
+        }
+    }
+}
